75c0037f153a80fb678913ee929b7c01547dc755
RaulOrtiz99 <raulalbertoortiz290199@hotmail.com>
2023-04-16 23:05:14 -0400

    agregando una nueva vista al proyecto

---
+++ lib/app/presentation/modules/splash/view/splash_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_meedu/ui.dart';
+import '../controller/splash_provider.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+  
--- lib/app/presentation/routes/app_routes.dart
+++ lib/app/presentation/routes/app_routes.dart
@@ -1,3 +1,4 @@
+import '../modules/splash/view/splash_view.dart';
 import 'package:flutter/widgets.dart' show BuildContext, Widget;
 import 'routes.dart';
 import '../modules/home/view/home_view.dart';
@@ -6,6 +7,6 @@ import '../modules/home/view/home_view.dart';
 Map<String, Widget Function(BuildContext)> get appRoutes {
   return {
     Routes.HOME: (_) => const HomeView(),
+    Routes.SPLASH: (_) => const SplashView(),
   };
 }
-    
